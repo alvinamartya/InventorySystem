e6147ff47f227a4d742a5ef598fe56fdbba29f68
alvinamartya <alvinamartya1@gmail.com>
2021-06-20 04:50:25 +0700

    add db

---
+++ inventory_system.sql
@@ -0,0 +1,611 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 19, 2021 at 11:50 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `inventory_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drivers`
+--
+
+CREATE TABLE `drivers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `vehicle_id` varchar(10) NOT NULL,
+  `warehouse_id` varchar(5) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `status` enum('A','D') NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_by` varchar(100) NOT NULL,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_by` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` varchar(30) NOT NULL,
+  `origin_id` varchar(6) NOT NULL,
+  `dest_warehouse_id` varchar(5) NOT NULL,
+  `origin_type` enum('Gudang','Pemasok') NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
+  `driver_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_by` varchar(100) NOT NULL,
+  `approved_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `approved_by` varchar(100) NOT NULL,
+  `checked_at` datetime DEFAULT current_timestamp(),
+  `checked_by` varchar(100) NOT NULL,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_by` varchar(100) NOT NULL,
+  `status_order_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_details`
+--
+
+CREATE TABLE `order_details` (
+  `id` int(11) NOT NULL,
+  `order_id` varchar(30) NOT NULL,
+  `product_id` int(5) NOT NULL,
+  `dest_shelf_id` varchar(20) NOT NULL,
+  `origin_shelf_id` varchar(20) DEFAULT NULL,
+  `status_product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `product_category_id` varchar(5) NOT NULL,
+  `units` varchar(100) NOT NULL,
+  `status` enum('A','D') NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_by` varchar(100) NOT NULL,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_by` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_categories`
+--
+
+CREATE TABLE `product_categories` (
+  `id` varchar(5) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `is_can_be_stale` int(11) NOT NULL,
+  `status` enum('A','D') NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_by` varchar(100) NOT NULL,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_by` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `retur`
+--
+
+CREATE TABLE `retur` (
+  `id` varchar(39) NOT NULL,
+  `origin_warehouse_id` varchar(5) NOT NULL,
+  `dest_id` varchar(6) NOT NULL,
+  `dest_type` enum('Gudang','Pemasok') NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
+  `driver_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_by` varchar(100) NOT NULL,
+  `approved_at` datetime DEFAULT current_timestamp(),
+  `approved_by` varchar(100) NOT NULL,
+  `checked_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `checked_by` int(11) NOT NULL,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_by` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `retur_detail`
+--
+
+CREATE TABLE `retur_detail` (
+  `id` int(11) NOT NULL,
+  `retur_id` varchar(30) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `origin_shelf_id` varchar(5) NOT NULL,
+  `dest_shelf_id` varchar(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shelf`
+--
+
+CREATE TABLE `shelf` (
+  `id` varchar(20) NOT NULL,
+  `warehouse_id` varchar(5) NOT NULL,
+  `type` enum('Rak Order','Rak Retur') NOT NULL,
+  `product_category_id` varchar(5) NOT NULL,
+  `rows` int(11) NOT NULL,
+  `columns` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_by` varchar(100) NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `updated_by` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shelf_details`
+--
+
+CREATE TABLE `shelf_details` (
+  `id` int(11) NOT NULL,
+  `shelf_id` varchar(20) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `expired_at` datetime DEFAULT NULL,
+  `row` int(11) NOT NULL,
+  `col` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staffs`
+--
+
+CREATE TABLE `staffs` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `gender` enum('M','F') NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `status` enum('A','D') NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_by` varchar(100) NOT NULL,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_by` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status_order`
+--
+
+CREATE TABLE `status_order` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `status_order`
+--
+
+INSERT INTO `status_order` (`id`, `name`) VALUES
+(1, 'Sedang Dikirim'),
+(2, 'Tiba Ditujuan'),
+(3, 'Sedang Dicek'),
+(4, 'Faktur Perlu Diperbarui'),
+(5, 'Berhasil di Approved');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status_product`
+--
+
+CREATE TABLE `status_product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `status_product`
+--
+
+INSERT INTO `status_product` (`id`, `name`) VALUES
+(1, 'Ok'),
+(2, 'Produk Mengalami Kerusakan'),
+(3, 'Produk Sudah Expired');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stores`
+--
+
+CREATE TABLE `stores` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `province` varchar(100) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `head_of_store_name` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `status` enum('A','D') NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_by` varchar(100) NOT NULL,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_by` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers`
+--
+
+CREATE TABLE `suppliers` (
+  `id` varchar(5) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `status` enum('A','D') NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_by` varchar(100) NOT NULL,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_by` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier_details`
+--
+
+CREATE TABLE `supplier_details` (
+  `id` int(11) NOT NULL,
+  `supplier_id` varchar(5) NOT NULL,
+  `product_category_id` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `warehouses`
+--
+
+CREATE TABLE `warehouses` (
+  `id` varchar(5) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `province` varchar(100) NOT NULL,
+  `is_branch` int(11) NOT NULL,
+  `status` enum('A','D') NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `created_by` varchar(100) NOT NULL,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_by` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `drivers`
+--
+ALTER TABLE `drivers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `warehouse_id` (`warehouse_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `dest_warehouse_id` (`dest_warehouse_id`),
+  ADD KEY `driver_id` (`driver_id`),
+  ADD KEY `status_order_id` (`status_order_id`);
+
+--
+-- Indexes for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `dest_shelf_id` (`dest_shelf_id`),
+  ADD KEY `origin_shelf_id` (`origin_shelf_id`),
+  ADD KEY `status_product_id` (`status_product_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_category_id` (`product_category_id`);
+
+--
+-- Indexes for table `product_categories`
+--
+ALTER TABLE `product_categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `retur`
+--
+ALTER TABLE `retur`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `origin_warehouse_id` (`origin_warehouse_id`),
+  ADD KEY `driver_id` (`driver_id`);
+
+--
+-- Indexes for table `retur_detail`
+--
+ALTER TABLE `retur_detail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `retur_id` (`retur_id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `origin_shelf_id` (`origin_shelf_id`),
+  ADD KEY `dest_shelf_id` (`dest_shelf_id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `shelf`
+--
+ALTER TABLE `shelf`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `warehouse_id` (`warehouse_id`),
+  ADD KEY `product_category_id` (`product_category_id`);
+
+--
+-- Indexes for table `shelf_details`
+--
+ALTER TABLE `shelf_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `shelf_id` (`shelf_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `staffs`
+--
+ALTER TABLE `staffs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- Indexes for table `status_order`
+--
+ALTER TABLE `status_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `status_product`
+--
+ALTER TABLE `status_product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stores`
+--
+ALTER TABLE `stores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `supplier_details`
+--
+ALTER TABLE `supplier_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supplier_id` (`supplier_id`),
+  ADD KEY `product_category_id` (`product_category_id`);
+
+--
+-- Indexes for table `warehouses`
+--
+ALTER TABLE `warehouses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `drivers`
+--
+ALTER TABLE `drivers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_details`
+--
+ALTER TABLE `order_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `retur_detail`
+--
+ALTER TABLE `retur_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `shelf_details`
+--
+ALTER TABLE `shelf_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `staffs`
+--
+ALTER TABLE `staffs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `status_order`
+--
+ALTER TABLE `status_order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `status_product`
+--
+ALTER TABLE `status_product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `stores`
+--
+ALTER TABLE `stores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `supplier_details`
+--
+ALTER TABLE `supplier_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `drivers`
+--
+ALTER TABLE `drivers`
+  ADD CONSTRAINT `drivers_ibfk_1` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouses` (`id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`dest_warehouse_id`) REFERENCES `warehouses` (`id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`),
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`status_order_id`) REFERENCES `status_order` (`id`);
+
+--
+-- Constraints for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  ADD CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `order_details_ibfk_3` FOREIGN KEY (`dest_shelf_id`) REFERENCES `shelf` (`id`),
+  ADD CONSTRAINT `order_details_ibfk_4` FOREIGN KEY (`origin_shelf_id`) REFERENCES `shelf` (`id`),
+  ADD CONSTRAINT `order_details_ibfk_5` FOREIGN KEY (`status_product_id`) REFERENCES `status_product` (`id`);
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`product_category_id`) REFERENCES `product_categories` (`id`);
+
+--
+-- Constraints for table `retur`
+--
+ALTER TABLE `retur`
+  ADD CONSTRAINT `retur_ibfk_1` FOREIGN KEY (`origin_warehouse_id`) REFERENCES `warehouses` (`id`),
+  ADD CONSTRAINT `retur_ibfk_2` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`);
+
+--
+-- Constraints for table `retur_detail`
+--
+ALTER TABLE `retur_detail`
+  ADD CONSTRAINT `retur_detail_ibfk_1` FOREIGN KEY (`retur_id`) REFERENCES `retur` (`id`),
+  ADD CONSTRAINT `retur_detail_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `retur_detail_ibfk_3` FOREIGN KEY (`origin_shelf_id`) REFERENCES `shelf` (`id`),
+  ADD CONSTRAINT `retur_detail_ibfk_4` FOREIGN KEY (`dest_shelf_id`) REFERENCES `shelf` (`id`);
+
+--
+-- Constraints for table `shelf`
+--
+ALTER TABLE `shelf`
+  ADD CONSTRAINT `shelf_ibfk_1` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouses` (`id`),
+  ADD CONSTRAINT `shelf_ibfk_2` FOREIGN KEY (`product_category_id`) REFERENCES `product_categories` (`id`);
+
+--
+-- Constraints for table `shelf_details`
+--
+ALTER TABLE `shelf_details`
+  ADD CONSTRAINT `shelf_details_ibfk_1` FOREIGN KEY (`shelf_id`) REFERENCES `shelf` (`id`),
+  ADD CONSTRAINT `shelf_details_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Constraints for table `staffs`
+--
+ALTER TABLE `staffs`
+  ADD CONSTRAINT `staffs_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
+
+--
+-- Constraints for table `supplier_details`
+--
+ALTER TABLE `supplier_details`
+  ADD CONSTRAINT `supplier_details_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`),
+  ADD CONSTRAINT `supplier_details_ibfk_2` FOREIGN KEY (`product_category_id`) REFERENCES `product_categories` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
